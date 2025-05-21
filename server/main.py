0b834cda1a57f6a0dcadc2937e753230387516a0
blendaabazi <ba64884@ubt-uni.net>
2025-05-21 16:02:03 +0200

    Auth View Model& SignUp/Login Update

--- server/main.py
+++ server/main.py
@@ -1,10 +1,28 @@
+# from fastapi import FastAPI
+# from routes import auth
+# from models.base import Base
+# from database import engine
+# app = FastAPI()
+
+# app.include_router(auth.router, prefix='/auth')
+
+
+# Base.metadata.create_all(engine)
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 from routes import auth
 from models.base import Base
 from database import engine
+
 app = FastAPI()
 
 app.include_router(auth.router, prefix='/auth')
 
-
 Base.metadata.create_all(engine)
+
+# ✅ Shto këtë për ta trajtuar GET /
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    return "<h1>Welcome to the API</h1>"
